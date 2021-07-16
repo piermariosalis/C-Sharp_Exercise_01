32c4555ceae6a22ebfc603764b773ff6c7981eb8
PierMario Salis <piermariosalis@MBPdiPierMario.fritz.box>
2021-07-16 11:32:42 +0200

    Exercise conversion Farhenheit to celsius

---
+++ exercise.cs
@@ -0,0 +1,32 @@
+// convert a temperature from degrees Fahrenheit to Celsius
+// Version 1
+
+/*
+int fahrenheit = 94;
+
+// to convert in Celsius, first subtract 32 and then moltiply five ninths (5 / 9). 
+
+int value = 32;
+
+int subtraction = fahrenheit - value;
+Console.WriteLine(subtraction);
+
+decimal division = 5m / 9m;
+Console.WriteLine(division);
+
+decimal result = subtraction * division;
+
+Console.WriteLine($@"The current temperature in Celsius is: {result}");
+*/
+
+// Version 2
+
+/*
+
+int fahrenheit = 94;
+
+decimal celsius = (fahrenheit - 32) * (5m/9m);
+
+Console.WriteLine($@"The current temperature in Celsius is: {celsius}");
+
+*/
